24ecaf70620609e2dd894efadafc19d2b22bea2c
islam <islam-sami999@knights.ucf.edu>
2019-10-19 23:22:40 -0400

    Neww

--- Aly_P2/src/savingsaccount/Mainclass.java
+++ Aly_P2/src/savingsaccount/Mainclass.java
@@ -1,65 +1,65 @@
 package savingsaccount;
 
 class SavingsAccount{
-    public static double annualInterestRate = 0.0; // static class variable
+    public static double annualInterestRate = 0.0;
 
     private double savingsBalance; // private instance variable
 
-    // Constructor
+
     public SavingsAccount(double amt){
         this.savingsBalance = amt;
     }
 
-    // to calculate the monthly interest
-    public void calculateMonthlyInterest(){
+
+    public void cMI(){
         double interest;
         interest = (this.savingsBalance * (annualInterestRate/100))/12;
         this.savingsBalance += interest;
     }
 
-    // to display balance
-    public void showBalance(){
+
+    public void showBal(){
         System.out.printf("$%.2f", this.savingsBalance);
     }
 
     // to set the static variable
-    public static void modifyInterestRate(double newRate){
+    public static void mIR(double newRate){
         annualInterestRate = newRate;
     }
 }
 
 public class Mainclass{
     public static void main(String[] args){
-        SavingsAccount saver1 = new SavingsAccount(2000.0); // initializing first instance
-        SavingsAccount saver2 = new SavingsAccount(3000.0); // initializing second instance
-        SavingsAccount.modifyInterestRate(4.0); // setting interest rate to 4.0%
+        SavingsAccount saver1 = new SavingsAccount(2000.0);
+        SavingsAccount saver2 = new SavingsAccount(3000.0);
+        SavingsAccount.mIR(4.0); // setting interest rate to 4.0%
         System.out.println("Month \t\t Saver1 \t Saver2");
 
 // Displaying data of two instances for 12 months
         for(int i=1; i<=12; ++i){
-            saver1.calculateMonthlyInterest();
-            saver2.calculateMonthlyInterest();
+            saver1.cMI();
+            saver2.cMI();
             System.out.print("Month " + i + ": \t");
-            saver1.showBalance();
+            saver1.showBal();
             System.out.print("\t");
-            saver2.showBalance();
+            saver2.showBal();
             System.out.println();
         }
         System.out.println();
 
-        System.out.println("Setting the Interest Rate to 5.0%");
+        System.out.println("Interest Rate now to 5.0%");
         System.out.println();
-        SavingsAccount.modifyInterestRate(5.0); // Changing the interest rate to 5.0%
+        SavingsAccount.mIR(5.0);
 
 // Calculating new Savings Balance
-        saver1.calculateMonthlyInterest();
-        saver2.calculateMonthlyInterest();
+        saver1.cMI();
+        saver2.cMI();
 
-// Displaying final data
+
         System.out.print("Month 12: \t");
-        saver1.showBalance();
+        saver1.showBal();
         System.out.print("\t");
-        saver2.showBalance();
+        saver2.showBal();
         System.out.println();
     }
 }
